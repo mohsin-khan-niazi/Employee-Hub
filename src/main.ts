a7b84d5170b5faf5389b5f725f7749bcd1828238
Mohsin <mohsinkniazi22@gmail.com>
2025-05-20 05:25:37 +0500

    feat: dark mode added to docs

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ import { AppModule } from './app.module';
 import validationOptions from './utils/validation-options';
 import { AllConfigType } from './config/config.type';
 import { ResolvePromisesInterceptor } from './utils/serializer.interceptor';
+import { SwaggerTheme } from 'swagger-themes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -43,8 +44,12 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
 
+  const swaggerThemes = new SwaggerTheme('v3');
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('docs', app, document, {
+    explorer: true,
+    customCss: swaggerThemes.getBuffer('dark'),
+  });
 
   await app.listen(configService.getOrThrow('app.port', { infer: true }));
 }
